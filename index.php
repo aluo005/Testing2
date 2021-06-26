d0d2a1e261e333178091a3d2185491d9ad2a881c
Albert Luo <albertluo040605@gmail.com>
2021-06-25 19:10:02 -0700

    moved to php

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
-sdfdsfdsf
-aaaaaaa
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chinese Kids Tutoring</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section class="header">
+            <nav>
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="">REGISTER</a></li>
+                        <li><a href="">ABOUT</a></li>
+                        <li><a href="">CONTACT</a></li>
+                    </ul>
+                </div>
+            </nav>
+        
+        <div class="text-box">
+            <h1>Chinese Kid Tutoring Group</h1>
+            <p>insert text here<br> insert text h ere</p>
+            <a href=""><button>Register Now</button></a>
+        </div>
+
+        </section>
+
+    </body>
+</html>

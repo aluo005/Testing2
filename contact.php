a3f9d3587358a02fb1f6dc1f95147bf188ff2f6f
ayangsea <ayang@overlake.org>
2021-06-27 20:29:55 -0700

    a

--- contact.php
+++ contact.php
@@ -28,7 +28,7 @@
             <div class="contact-info">
                 <ul>
                     <li>
-                        <h2>Name</h2><br><p>email<br>insert wechat id</p>
+                        <h2>NameName</h2><br><p>email<br>insert wechat id</p>
                     </li>
                     <br>
                     <li>
